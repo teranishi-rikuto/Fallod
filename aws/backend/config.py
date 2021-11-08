cae444e856bdbea98a24cabe885ac307ed85e65a
urasaki <urasakikeisuke.ml@gmail.com>
2021-11-09 02:04:43 +0900

    wip

--- aws/backend/config.py
+++ aws/backend/config.py
@@ -8,6 +8,6 @@ from constants import *
 config: Dict[str, Union[str, int]] = {
     CONFIG_RATE_LIMITS: "1000 per minute",
     CONFIG_DANGER_AREA_RANGE: 10,
-    CONFIG_MAP_HTML_PATH: "../frontend/templates/map.html",
+    CONFIG_MAP_HTML_PATH: "../frontend/templates",
 }
 
