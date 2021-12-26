4ba2c33b39ae8da639b462f38bc1494ed45524fe
urasakikeisuke <79469739+urasakikeisuke@users.noreply.github.com>
2021-12-26 11:07:49 +0900

    Update install.sh

--- aws/install.sh
+++ aws/install.sh
@@ -4,7 +4,7 @@ sudo apt update
 sudo apt install -y \
     python3-pip
 
-git config --global user.name "urasaki"
-git config --global user.email 213427006@ccmailg.meijo-u.ac.jp
+git config --local user.name "sia"
+git config --local user.email hello@world.com
 
 pip3 install -r requirements.txt
